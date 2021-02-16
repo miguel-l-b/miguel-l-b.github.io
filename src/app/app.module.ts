df7d32cd50a435f57477ae5cade9f2e6bfe1634c
Miguel-Coruj <miguelcoruj@gmail.com>
2021-02-16 15:04:49 -0300

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectComponent } from './project/project.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ComponentToolbarComponent } from './component-toolbar/component-toolbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    PageNotFoundComponent,
+    ComponentToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    MatIconModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
