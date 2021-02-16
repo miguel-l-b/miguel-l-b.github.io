a22a32bc22206225c483f42f54da7980e6289b8e
Miguel-Coruj <miguelcoruj@gmail.com>
2021-02-16 15:06:17 -0300

    toolbar

---
+++ src/app/component-toolbar/component-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-toolbar',
+  templateUrl: './component-toolbar.component.html',
+  styleUrls: ['../../assets/css/toolbar.css']
+})
+export class ComponentToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
